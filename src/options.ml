0ef5b3a7559976bf185db53a01ba0c50983967df
Johannes Kanig <johannes.kanig@lri.fr>
2010-02-24 09:55:59 +0100

    an option for versions

--- src/options.ml
+++ src/options.ml
@@ -14,21 +14,27 @@ let verbose = ref false
 let transform_only = ref false
 let no_check = ref false
 
-let transforms = 
-  ref (List.rev  [ Anf.theory ; Wp.theory ; InlineLet.theory ; Simplify.theory ])
+let transforms =
+  ref (List.rev [ Anf.theory ; Wp.theory ; InlineLet.theory ; Simplify.theory ])
 
 let append_trans x () = transforms := x :: !transforms
 
 let clear () = transforms := []
 
-let opt_spec = 
+let print_version () =
+  Format.printf "%s" Version.version; exit 0
+
+let opt_spec =
   Arg.align
-  [ 
-    "--input-annot", Arg.Set input_annot, "take fully type annotated input file";
+  [
+    "--input-annot", Arg.Set input_annot,
+      " take fully type annotated input file";
     "--parse-only", Arg.Set parse_only, " parse file and exit";
     "--infer-only", Arg.Set infer_only, " do type inference and exit";
-    "--transform-only", Arg.Set transform_only, " stop after applying transforms";
-    "--no-check", Arg.Set no_check, " do not execute type checks after transforms";
+    "--transform-only", Arg.Set transform_only,
+      " stop after applying transforms";
+    "--no-check", Arg.Set no_check,
+      " do not execute type checks after transforms";
     "--clear", Arg.Unit clear, " clear the list of transformations";
     "--anf", Arg.Unit (append_trans Anf.theory),
       " apply anf normal form transformation";
@@ -38,25 +44,26 @@ let opt_spec =
       " inline let bindings";
     "--mapsimpl", Arg.Unit (append_trans Simplify.theory),
       " simplify map expressions";
-    "-o", Arg.Set_string outfile, 
+    "-o", Arg.Set_string outfile,
             "<arg> use <arg> instead of default filename for output";
-    "--pangoline", Arg.Unit (fun () -> backend := `Pangoline), 
+    "--pangoline", Arg.Unit (fun () -> backend := `Pangoline),
             " set output format to pangoline";
-    "--coq", Arg.Unit (fun () -> backend := `Coq), 
+    "--coq", Arg.Unit (fun () -> backend := `Coq),
             " set output format to Coq";
     "--check-coq", Arg.Set check_coq, " check produced coq file using coqc";
     "--noprelude", Arg.Set no_prelude, " do not add a prelude to the file";
     "-v", Arg.Set verbose, " be verbose";
     "--verbose", Arg.Set verbose, " be verbose";
+    "--version", Arg.Unit print_version , " print version information and exit";
   ]
 
 let () = Arg.parse opt_spec store_fn "Usage: who <options> <file>"
 
-let update () = 
-  let base = 
+let update () =
+  let base =
     match !filename with
     | None -> "base"
     | Some s -> Filename.chop_extension s in
-  let () = 
+  let () =
     suffix := match !backend with | `Pangoline -> ".pge" | `Coq -> ".v" in
   if !outfile = "" then outfile := Myformat.sprintf "%s_who%s" base !suffix
