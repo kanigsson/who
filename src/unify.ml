20c1aa072d1972ba723a1648792b1007f333c35f
Johannes Kanig <johannes.kanig@lri.fr>
2010-03-16 17:23:52 +0100

    TODO

--- src/unify.ml
+++ src/unify.ml
@@ -24,20 +24,20 @@
 module Uf = Unionfind
 module PT = Predefined.Ty
 
-type ty = 
+type ty =
   | U
   | T of (node,rnode, effect) Ty.t'
 and node = ty Uf.t
 and rnode = r Uf.t
-and r = 
-  | RU 
+and r =
+  | RU
   | RT of Name.t
 and effect = rnode list * Name.S.t
 
 let new_ty () = Uf.fresh U
 let mkt t = Uf.fresh (T t)
 let arrow t1 t2 e c = mkt (Ty.Arrow (t1,t2,e,c))
-let tuple t1 t2 = mkt (Ty.Tuple (t1,t2)) 
+let tuple t1 t2 = mkt (Ty.Tuple (t1,t2))
 let ref_ r t = mkt (Ty.Ref (r,t))
 let mkr r = Uf.fresh (RT r)
 let new_r () = Uf.fresh RU
@@ -48,15 +48,15 @@ let parr t1 t2 = mkt (Ty.PureArr (t1,t2))
 
 let eff_empty = [], Name.S.empty
 
-let r_equal r1 r2 = 
+let r_equal r1 r2 =
   match Uf.desc r1, Uf.desc r2 with
   | RU, RU -> Uf.equal r1 r2
   | RU, RT _ | RT _, RU -> false
   | RT n1, RT n2 -> Name.equal n1 n2
 
-let rremove (r,e) rl = 
+let rremove (r,e) rl =
   List.filter (fun x -> not (Misc.list_mem r_equal x rl)) r, e
-let eff_union (r1,e1) (r2,e2) = 
+let eff_union (r1,e1) (r2,e2) =
   Misc.list_union r_equal r1 r2, Name.S.union e1 e2
 
 let eff_union3 a b c = eff_union a (eff_union b c)
@@ -65,7 +65,7 @@ let eff_union3 a b c = eff_union a (eff_union b c)
 open Const
 let const =
   let h = Hashtbl.create 5 in
-  List.iter (fun c -> Hashtbl.add h c (mkt (Ty.Const c))) 
+  List.iter (fun c -> Hashtbl.add h c (mkt (Ty.Const c)))
   [ TInt ; TProp ];
   fun c -> Hashtbl.find h c
 
@@ -76,20 +76,20 @@ let int = const TInt
 
 let union a b = Uf.union (fun a _ -> a) a b
 open Myformat
-let rec print_node fmt x = 
+let rec print_node fmt x =
   match Uf.desc x with
   | U -> fprintf fmt "%d" (Uf.tag x)
-  | T t -> 
+  | T t ->
       Ty.print' ~kind:`Who print_node prvar preff is_c fmt t
-and is_c x = 
+and is_c x =
   match Uf.desc x with
   | U -> false
   | T t -> Ty.is_compound t
-and prvar fmt x = 
+and prvar fmt x =
   match Uf.desc x with
   | RU -> fprintf fmt "%d" (Uf.tag x)
   | RT x -> Name.print fmt x
-and preff fmt (rl,el) = 
+and preff fmt (rl,el) =
   fprintf fmt "{%a|" (print_list space prvar) rl;
   Name.S.iter (Name.print fmt) el;
   pp_print_string fmt "}"
@@ -110,7 +110,7 @@ let rec unify a b =
       | Ty.PureArr (ta1,ta2), Ty.PureArr (tb1,tb2)
       | Ty.Arrow (ta1,ta2,_,_), Ty.PureArr (tb1,tb2)
       | Ty.PureArr (ta1,ta2), Ty.Arrow (tb1,tb2,_,_)
-      | Ty.Tuple (ta1,ta2), Ty.Tuple (tb1,tb2) -> 
+      | Ty.Tuple (ta1,ta2), Ty.Tuple (tb1,tb2) ->
           unify ta1 tb1;
           unify ta2 tb2
       | Ty.Arrow (ta1,ta2,e1,c1), Ty.Arrow (tb1,tb2,e2,c2) ->
@@ -123,11 +123,11 @@ let rec unify a b =
       | Ty.App (v1,i1), Ty.App (v2,i2) when v1 = v2 ->
           Inst.iter2 unify runify eunify i1 i2
       | _ , _ -> raise CannotUnify
-      end; 
+      end;
       (* We really have to unify afterwards, in case of an exception being
        * raised *)
       union a b;
-and runify a b = 
+and runify a b =
 (*   printf "runify: %a and %a@." prvar a prvar b; *)
   if Uf.equal a b then () else
   match Uf.desc a, Uf.desc b with
@@ -135,14 +135,14 @@ and runify a b =
   | RU , RT _ -> union b a
   | RT _, RU -> union a b
   | RT s1, RT s2 when s1 = s2 -> ()
-  | RT _, RT _ -> 
+  | RT _, RT _ ->
 (*       printf "runify: %a and %a@." prvar a prvar b; *)
       raise CannotUnify
-and eunify (r1,e1) (r2,e2) = 
+and eunify (r1,e1) (r2,e2) =
   if Effect.s_equal e1 e2 && Misc.list_equal_unsorted r_equal r1 r2 then ()
   else raise CannotUnify
-      
-module H = Hashtbl.Make (struct 
+
+module H = Hashtbl.Make (struct
                            type t = node
                            let equal a b = Uf.tag a = Uf.tag b
                            let hash = Uf.tag
@@ -151,23 +151,23 @@ module H = Hashtbl.Make (struct
 let to_ty, to_eff, to_r =
   let h = H.create 127 in
   let rec ty' : (node, rnode, effect) Ty.t' -> Ty.t = function
-    | Ty.Arrow (t1,t2,e,cap) -> 
+    | Ty.Arrow (t1,t2,e,cap) ->
         Ty.caparrow (ty t1) (ty t2) (eff e) (List.map rv cap)
     | Ty.Tuple (t1,t2) -> Ty.tuple (ty t1) (ty t2)
     | Ty.Const c -> Ty.const c
     | Ty.Ref (r,t) -> Ty.ref_ (rv r) (ty t)
     | Ty.Map e -> Ty.map (eff e)
     | Ty.PureArr (t1,t2) -> Ty.parr (ty t1) (ty t2)
-    | Ty.App (v,i) -> Ty.app v (Inst.map ty rv eff i) 
-  and ty x = 
-    try H.find h x 
-    with Not_found -> 
+    | Ty.App (v,i) -> Ty.app v (Inst.map ty rv eff i)
+  and ty x =
+    try H.find h x
+    with Not_found ->
       match Unionfind.desc x with
       | U -> assert false
-      | T t -> 
+      | T t ->
           let r = ty' t in
           H.add h x r; r
-  and rv r = 
+  and rv r =
     match Uf.desc r with
     | RU -> assert false
     | RT s -> s
