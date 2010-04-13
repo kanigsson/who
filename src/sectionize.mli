4948b8f53e046fb887ccee9551ed7289d2e67321
Johannes Kanig <johannes.kanig@lri.fr>
2010-04-13 16:28:57 +0200

    headers and .mli file

--- src/sectionize.mli
+++ src/sectionize.mli
@@ -21,4 +21,4 @@
 (*  along with this program.  If not, see <http://www.gnu.org/licenses/>      *)
 (******************************************************************************)
 
-val theory : Ast.theory -> Ast.theory list
+val theory : Ast.theory -> Ast.theory
