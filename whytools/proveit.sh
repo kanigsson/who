f8f146520b3e4fee64fc6a1046684c137c91a7f0
Johannes Kanig <johannes.kanig@lri.fr>
2010-04-14 15:42:54 +0200

    simplify proveit; getopt just doesnt work

--- whytools/proveit.sh
+++ whytools/proveit.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-
 set -x
-TIMEOUT=5
-CORES=5
-while getopts 't:c:' OPTION
-do
-  case $OPTION in
-    t) TIMEOUT=$OPTARG ;;
-    c) CORES=$OPTARG;;
-    ?) exit 2;;
-  esac
-done
-
-shift $(( $(OPTIND) - 1 ))
-
+if [ -z "$TIMEOUT" ] ; then
+  TIMEOUT=5
+fi
+if [ -z "$CORES" ] ; then
+  CORES=5
+fi
 CAT=`date +%F+%X`
 proofmgr -add-bench -c $CAT $*
 PROVERS=`proofmgr -show-provers | grep "\[.*\]" | sed -e "s/.*\[.\(.*\)\].*/\1/"`
 for i in $PROVERS ; do
-  proofmgr -run -i $i -c $CAT -timeout $TIMEOUT
+  proofmgr -run -i $i -c $CAT -timeout $TIMEOUT -cores $CORES
 done
 
