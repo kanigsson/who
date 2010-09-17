f477e50d28aae43420838178a8a661ee3d103702
Johannes Kanig <johannes.kanig@lri.fr>
2010-09-17 16:25:23 +0200

    ymemo and fix for proveit

--- whytools/proveit.sh
+++ whytools/proveit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PROOFMGR="proofmgr-nogui"
 set -x
 if [ -z "$TIMEOUT" ] ; then
   TIMEOUT=5
@@ -7,9 +8,9 @@ if [ -z "$CORES" ] ; then
   CORES=5
 fi
 CAT=`date +%F+%X`
-proofmgr -add-bench -c $CAT $*
-PROVERS=`proofmgr -show-provers | grep "\[.*\]" | sed -e "s/.*\[.\(.*\)\].*/\1/"`
+$PROOFMGR -add-bench -c $CAT $*
+PROVERS=`$PROOFMGR -show-provers | grep "\[.*\]" | sed -e "s/.*\[.\(.*\)\].*/\1/"`
 for i in $PROVERS ; do
-  proofmgr -run -i $i -c $CAT -timeout $TIMEOUT -cores $CORES
+  $PROOFMGR -run -i $i -c $CAT -timeout $TIMEOUT -cores $CORES
 done
 
